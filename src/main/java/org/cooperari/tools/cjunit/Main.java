45a21d9bed11658e54404c8fd868e537addd39fb
Eduardo Marques <edrdo@dcc.fc.up.pt>
2019-01-26 12:00:11 +0000

    cjunitp script + adjustments to cjunit program logic

--- src/main/java/org/cooperari/tools/cjunit/Main.java
+++ src/main/java/org/cooperari/tools/cjunit/Main.java
@@ -3,8 +3,8 @@ package org.cooperari.tools.cjunit;
 import java.io.File;
 import java.io.PrintStream;
 
+import org.cooperari.CSystem;
 import org.cooperari.core.CWorkspace;
-import org.cooperari.core.aspectj.AgentFacade;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 
@@ -13,28 +13,35 @@ import org.junit.runner.Result;
  * 
  * @since 0.2
  */
-public class Main {
+public final class Main {
 
   /**
-   * Private constructor to avoid instantiation.
+   * Exit code when all tests passed.
    */
-  private Main() {
-  }
-
+  private static final int ALL_TESTS_PASSED = 0;
+  
+  /**
+   * Exit code when some tests failed.
+   */
+  private static final int SOME_TESTS_FAILED = 1;
+  
+  /**
+   * Exit code for invalid arguments.
+   */
+  private static final int INVALID_ARGUMENTS = 2;
+  
+  /**
+   * Exit code for unexpected internal error.
+   */
+  private static final int INTERNAL_ERROR = 3;
+  
   /**
    * Program entry point. 
    * 
    * @param args Arguments.
    */
   public static void main(String[] args) {
-    int exitCode;
-    try {
-      exitCode = execute(args, System.out);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      System.out.println("Unexpected error.");
-      exitCode = 2;
-    }
+    int exitCode = execute(args, System.out);
     System.exit(exitCode);
   }
 
@@ -49,28 +56,52 @@ public class Main {
    *         non-zero in the case of errors.
    */
   public static int execute(String[] args, PrintStream out) {
-    try {      
-      if (! AgentFacade.INSTANCE.isActive()) {
-        out.println("The AspectJ load-time weaver does not seem to be active!");
-        return 2;
+    try {    
+      if (args == null || args.length <= 2) {
+        out.println("Invalid arguments!");
+        return INVALID_ARGUMENTS;
+      }
+      if (! CSystem.inCooperativeMode()) {
+        out.println("Execution will be preemptive, AspectJ LTW is not active.");
       }
       
-      if (!CWorkspace.INSTANCE.isInitialized()) {
-        CWorkspace.INSTANCE.initialize(new File("."));
+      if (! CWorkspace.INSTANCE.isInitialized()) {
+        File root_dir = new File(args[0]);
+        if (!root_dir.isDirectory()) {
+          out.println(root_dir.getAbsolutePath() + " is not a directory!");
+          return INVALID_ARGUMENTS;
+        }
+        CWorkspace.INSTANCE.initialize(root_dir);
       }
       
-      Class<?>[] classes = new Class<?>[args.length];
-      for (int i = 0; i < args.length; i++) {
-        classes[i] = Class.forName(args[i]);
+      Class<?>[] classes = new Class<?>[args.length-1];
+      for (int i = 1; i < args.length; i++) {
+        try {
+          classes[i-1] = Class.forName(args[i]);
+        }
+        catch (ClassNotFoundException e) {
+          out.println(args[i] + ": class not found!");
+          return INVALID_ARGUMENTS;
+        }
       }
       
       JUnitCore juc = new JUnitCore();
       juc.addListener(new CJUnitRunListener(System.out));
       Result r = juc.run(classes);   
-      return r.getFailureCount() == 0 ? 0 : 1;
-    } catch (Throwable e) {
+      return r.getFailureCount() == 0 ? ALL_TESTS_PASSED : SOME_TESTS_FAILED;
+    } 
+    catch (Throwable e) {
+      out.println("Unexpected error!");
       e.printStackTrace(out);
-      return 3;
+      return INTERNAL_ERROR;
     }
   }
+  
+  /**
+   * Private constructor to avoid instantiation.
+   */
+  private Main() {
+    
+  }
+
 }
