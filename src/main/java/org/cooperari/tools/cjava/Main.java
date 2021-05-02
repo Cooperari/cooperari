31355ae2600ea4d733d0f604436c1d82084cb228
Eduardo Marques <edrdo@dcc.fc.up.pt>
2021-05-02 23:02:29 +0100

    cjava and cjavap scripts (replacing capp)

--- src/main/java/org/cooperari/tools/cjava/Main.java
+++ src/main/java/org/cooperari/tools/cjava/Main.java
@@ -13,7 +13,7 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 //
-package org.cooperari.tools.capp;
+package org.cooperari.tools.cjava;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
